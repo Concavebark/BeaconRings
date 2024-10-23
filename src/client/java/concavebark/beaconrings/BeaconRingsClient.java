923cfc9458a89daaff6c1c67eba1ebb83adc05a9
Concavebark <concavebarkplaysgames@yahoo.com>
2024-10-23 16:58:18 -0400

    init commit

--- src/client/java/concavebark/beaconrings/BeaconRingsClient.java
+++ src/client/java/concavebark/beaconrings/BeaconRingsClient.java
@@ -1,10 +1,18 @@
 package concavebark.beaconrings;
 
 import net.fabricmc.api.ClientModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class BeaconRingsClient implements ClientModInitializer {
+	public static final String MOD_ID = "beaconrings";
+
+	public static final Logger CLIENT_LOGGER = LoggerFactory.getLogger(MOD_ID);
+
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+		CLIENT_LOGGER.info("Beacon Rings Client Loading!");
 	}
 }
